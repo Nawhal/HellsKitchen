c92fa324a650f355ee2bc57165406c2afdf251bc
Camille <camille.dresse@gmail.com>
2017-03-27 15:53:57 +0200

    Update stats.php

--- PHP/stats.php
+++ PHP/stats.php
@@ -1,4 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 
 if($resultviande[0]<$resultpoisson[0]) echo("Les plats à base de poisson sont plus consommés");
 else echo("Les plats à base de viande sont plus consommés");
+?>
+</body>
+</html>
+
+
+
+ 
