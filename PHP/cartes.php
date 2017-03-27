93a821f35c6378180b32157c57c9a1e7f8ab4917
Zanorin <evalukario@hotmail.fr>
2017-03-27 16:16:39 +0200

    Add files via upload

---
+++ PHP/cartes.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<!--<?php /*require_once('./header.php');*/ ?>-->
+</head>
+
+<body>
+
+	<div>
+		<h2>Cartes</h2>
+		<ul>
+			<?php
+				foreach($res as $key => $val)
+				{
+					echo '<li> <a href=\'./view/cartes.php?id='.$val['idrestaurant'].'\'>'.$val['nomrestaurant'].'</a> a '.$val['ville'].', '.$val['pays'].' - '.$val['adresse'].'</li>';
+				}
+			?>
+		</ul>
+	</div>
+
+</body>
+
+</html>
