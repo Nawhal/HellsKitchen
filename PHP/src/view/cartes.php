6d690cbbbe1fb21f0c63e988fa92f44325779052
Zanorin <evalukario@hotmail.fr>
2017-03-28 12:08:14 +0200

    Update cartes.php

--- PHP/src/view/cartes.php
+++ PHP/src/view/cartes.php
@@ -1,20 +1,18 @@
 <html lang="fr">
     <head>
         <?php
-            require_once './src/view/header.php';
+            require_once 'header.php';
         ?>
     </head>
     <body>
 	<div>
             <h1>Cartes</h1>
-            <ul>
                 <?php
                     foreach($res as $key => $val)
                     {
-                        echo '<li> <a href=\'./view/cartes.php?id='.$val['idRestaurant'].'\'>'.$val['nomRestaurant'].'</a> a '.$val['ville'].', '.$val['pays'].' - '.$val['adresse'].'</li>';
+                        			
                     }
                 ?>
-            </ul>
 	</div>
     </body>
 </html>
