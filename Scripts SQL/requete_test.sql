90e2aaea21be121f31649ddbad6691b8aae6c6e9
Camille <camille.dresse@gmail.com>
2017-03-30 18:37:43 +0200

    Update requete_test.sql

--- Scripts SQL/requete_test.sql
+++ Scripts SQL/requete_test.sql
@@ -1,5 +1,5 @@
 //Test pour trouver le prix des dessert commandes
-//fonctionne bizzarement
-select plat.nomplat, prixElement.prixElement, quantiteElement FROM commande,quantiteElement,element,plat,carte,prixElement, periodeCarte WHERE commande.idRestaurant=1 AND commande.idCommande=quantiteElement.idCommande AND quantiteElement.idElement=element.idElement AND element.idElement=plat.idElement AND plat.dessert=true AND element.idElement=prixElement.idElement ;
 
-select plat.nomplat, prixElement.prixElement FROM commande,quantiteElement,element,plat,carte,prixElement, periodeCarte WHERE commande.idRestaurant=1 AND commande.idCommande=quantiteElement.idCommande AND quantiteElement.idElement=element.idElement AND element.idElement=plat.idElement AND plat.dessert=true AND element.idElement=prixElement.idElement ;
+//il reste a faire en php la moyenne du coup et on obtiendra la somme que depense en moyenne les clients pout les dessert. (C'est un peu different de ce qui est demande car c'est par par commande mais pour le restau)
+
+select distinct plat.nomplat, prixElement.prixElement, quantiteElement.quantite FROM commande,quantiteElement,element,plat,carte,prixElement, periodeCarte WHERE commande.idRestaurant=1 AND commande.idCommande=quantiteElement.idCommande AND quantiteElement.idElement=element.idElement AND element.idElement=plat.idElement AND plat.dessert=true AND element.idElement=prixElement.idElement ;
